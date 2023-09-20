c2a6ee962da5366f25e94ff838a9dd13e96c9ed1
Dimitri Kurashvili <dimakura@gmail.com>
2023-09-20 14:42:08 +0400

    Fixes in parentFolder

--- src/parentFolder.js
+++ src/parentFolder.js
@@ -1,5 +1,13 @@
 import path from "path";
 
-export default function parentFolder(dir = __dirname) {
-  return dir.split(path.sep).slice(0, -1).join(path.sep);
+export default function parentFolder(dir, sep = path.sep) {
+  const parts = dir.split(sep);
+
+  const res = parts.pop();
+
+  if (parts.length > 1 && res === "") parts.pop();
+
+  if (parts.length === 1 && parts[0] === "") return sep;
+
+  return parts.join(sep);
 }
